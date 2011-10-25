c724b46da77bc487f16729e65b6c7b91572ef9c0
Javier Acero <j4cegu@gmail.com>
2011-10-25 15:42:31 +0200

    null post code from mog

---
+++ lib/null_post.rb
@@ -0,0 +1,10 @@
+require 'post'
+
+class NullPost < Post
+  PUBLICATION_TIME = DateTime.parse('99999-12-31 23:59:59+00:00')
+
+  def initialize
+    @title, @description, @content = '', '', ''
+    @publication_time = PUBLICATION_TIME
+  end
+end
